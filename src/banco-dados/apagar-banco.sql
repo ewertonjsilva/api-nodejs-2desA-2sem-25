156f9f3cefa8bf71e7e1e315904e0e302c4c2bed
Ewerton José da Silva <62845555+ewertonjsilva@users.noreply.github.com>
2025-11-19 20:26:18 -0300

    banco de dados criado

---
+++ src/banco-dados/apagar-banco.sql
@@ -0,0 +1,12 @@
+-- DROP DE TODAS AS TABELAS NA ORDEM DE EXCLUSÃO
+DROP TABLE pedido_produtos;
+DROP TABLE produto_ingredientes;
+DROP TABLE produtos;
+DROP TABLE produto_tipos; 
+DROP TABLE mesas; 
+DROP TABLE pedidos; 
+DROP TABLE cliente_enderecos; 
+DROP TABLE cidades;
+DROP TABLE clientes; 
+DROP TABLE usuarios;
+DROP TABLE ingredientes; 
